ba1f37b2f7104a0a3a83d053de368a62c81d0496
Wouter Smaardijk <wouter.smaardijk@maxict.nl>
2024-04-29 11:13:14 +0200

    Added additional html tags to be converted to adf

--- src/HtmlConverter.php
+++ src/HtmlConverter.php
@@ -2,7 +2,6 @@
 
 namespace Zeltotech\AdfToHtml;
 
-use JiraCloud\ADF\AtlassianDocumentFormat;
 use DOMDocument;
 
 class HtmlConverter
@@ -14,7 +13,7 @@ class HtmlConverter
         $this->content = $html;
     }
 
-    public function toAdf():array
+    public function toAdf(): array
     {
         $dom = new DOMDocument();
         @$dom->loadHTML($this->content);
@@ -25,106 +24,108 @@ class HtmlConverter
             "content" => []
         ];
 
-        foreach ($dom->getElementsByTagName('body')->item(0)->childNodes as $node) {
-            if ($node->nodeType === XML_ELEMENT_NODE) {
-                switch ($node->nodeName) {
-                    case 'p':
-                        $adf['content'][] = $this->convertParagraph($node);
-                        break;
-                    case 'h1':
-                    case 'h2':
-                    case 'h3':
-                    case 'h4':
-                    case 'h5':
-                    case 'h6':
-                        $adf['content'][] = $this->convertHeading($node);
-                        break;
-                }
-            }
+        $body = $dom->getElementsByTagName('body')->item(0) ?: $dom->documentElement;
+
+        foreach ($body->childNodes as $node) {
+            $this->processNode($node, $adf['content']);
         }
+
         return $adf;
     }
 
-    public function toStdClass(): \stdClass
+    private function processNode($node, &$content)
     {
-        $adfArray = $this->toAdf();
-        return json_decode(json_encode($adfArray));
+        if ($node->nodeType === XML_ELEMENT_NODE) {
+            switch ($node->nodeName) {
+                case 'a':
+                    $content[] = $this->convertLink($node);
+                    break;
+                case 'p':
+                case 'div':
+                    $content[] = $this->convertParagraph($node);
+                    break;
+                case 'h1':
+                case 'h2':
+                case 'h3':
+                case 'h4':
+                case 'h5':
+                case 'h6':
+                    $content[] = $this->convertHeading($node);
+                    break;
+                case 'ul':
+                case 'ol':
+                    $content[] = $this->convertList($node, $node->nodeName === 'ul' ? 'bulletList' : 'orderedList');
+                    break;
+            }
+        }
     }
 
     private function convertParagraph($node): array
     {
-        $content = [];
-
-        foreach ($node->childNodes as $child) {
-            if ($child->nodeType === XML_TEXT_NODE) {
-                $content[] = [
-                    "type" => "text",
-                    "text" => $child->nodeValue
-                ];
-            } elseif ($child->nodeType === XML_ELEMENT_NODE && $child->nodeName === 'strong') {
-                $content[] = [
-                    "type" => "text",
-                    "text" => $child->textContent,
-                    "marks" => [
-                        ["type" => "strong"]
-                    ]
-                ];
-            }
-        }
-
+        $content = $this->getNodeContent($node);
         return [
             "type" => "paragraph",
             "content" => $content
         ];
     }
 
-    private function convertHeading($node) {
+    private function convertHeading($node)
+    {
         $level = substr($node->nodeName, 1, 1);
-        $content = $this->convertNodeChildren($node);
+        $content = $this->getNodeContent($node);
 
         return [
             "type" => "heading",
-            "attrs" => [
-                "level" => (int)$level
-            ],
+            "attrs" => ["level" => (int)$level],
             "content" => $content
         ];
     }
 
-    private function convertNodeChildren($node): array
+    private function convertList($node, $type)
     {
-        $content = [];
-
+        $items = [];
         foreach ($node->childNodes as $child) {
-            if ($child->nodeType === XML_TEXT_NODE) {
-                $content[] = [
-                    "type" => "text",
-                    "text" => $child->nodeValue
-                ];
-            } elseif ($child->nodeType === XML_ELEMENT_NODE) {
-                switch ($child->nodeName) {
-                    case 'strong':
-                        $content[] = [
-                            "type" => "text",
-                            "text" => $child->textContent,
-                            "marks" => [
-                                ["type" => "strong"]
-                            ]
-                        ];
-                        break;
-                    case 'em':
-                        $content[] = [
-                            "type" => "text",
-                            "text" => $child->textContent,
-                            "marks" => [
-                                ["type" => "em"]
-                            ]
-                        ];
-                        break;
-                }
+            if ($child->nodeName === 'li') {
+                $items[] = $this->convertListItem($child);
             }
         }
+        return [
+            "type" => $type,
+            "content" => $items
+        ];
+    }
 
+    private function convertListItem($node)
+    {
+        $content = $this->getNodeContent($node);
+        return [
+            "type" => "listItem",
+            "content" => [$content]
+        ];
+    }
+
+    private function convertLink($node)
+    {
+        $href = $node->getAttribute('href');
+        return [
+            "type" => "text",
+            "text" => $node->textContent,
+            "marks" => [
+                ["type" => "link", "attrs" => ["href" => $href]]
+            ]
+        ];
+    }
+
+    private function getNodeContent($node)
+    {
+        $content = [];
+        foreach ($node->childNodes as $child) {
+            if ($child->nodeType === XML_TEXT_NODE && trim($child->nodeValue) !== '') {
+                $content[] = ["type" => "text", "text" => trim($child->nodeValue)];
+            } elseif ($child->nodeType === XML_ELEMENT_NODE) {
+                $this->processNode($child, $content);  // Recursive call to handle nested elements
+            }
+        }
         return $content;
     }
 }
